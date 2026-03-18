171f91c79a2fdd9f83c2ab50107f3d3e73834c48
Juan Marco Gosselin Gamboa <juanmarcogg@hotmail.com>
2026-03-18 12:56:06 -0600

    feat(Asistencia): Registro de asistencia y salida)

--- Profesores/urls.py
+++ Profesores/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.dashboard, name="profesores_dashboard"),
     path("registro-asistencia/", views.registro_asistencia, name="profesores_registro_asistencia"),
+    path("registrar-asistencia/", views.registrar_asistencia, name="profesores_registrar_asistencia"),
+    path("registrar-salida/", views.registrar_salida, name="profesores_registrar_salida"),
 ]
